757f42ec92628bd6c0afd862dd323f8cd684f8f8
Hery Nugroho <herynugie@gmail.com>
2025-05-21 16:10:39 +0700

    new_face

--- frontend/src/navigation/peny/index.js
+++ frontend/src/navigation/peny/index.js
@@ -5,36 +5,40 @@ export default [
     icon: 'HomeIcon',
   },
   {
-    title: 'Pesan',
+    title: 'Wablas SPMB 2025',
+    route: 'chat-app',
     icon: 'MessageCircleIcon',
     action: 'read', 
     resource: 'ACL',
-    children: [
-      {
-        title: 'Incoming Whatsapp',
-        route: 'listwa',
-      },
-      // {
-      //   title: 'WhatsApp Masuk',
-      //   route: 'daftar_pesan',
-      //   action: 'read', 
-      //   resource: 'ACL',
-      // },
-      {
-        title: 'F.A.Q',
-        route: 'faq',
-        action: 'read', 
-        resource: 'ACL',
-      },
-      {
-        title: 'Wablas SPMB 2025',
-        route: 'chat-app',
-        component: () => import('@/views/ChatApp.vue'),
-      },
-      // {
-      //   title: 'Chat',
-      //   route: 'chat',
-      // },
-    ]
+    // children: [
+    //   {
+    //     title: 'Wablas SPMB 2025',
+    //     route: 'chat-app',
+    //     component: () => import('@/views/ChatApp.vue'),
+    //   },
+    //   // {
+    //   //   title: 'Incoming Whatsapp',
+    //   //   route: 'listwa',
+    //   // },
+    //   // {
+    //   //   title: 'WhatsApp Masuk',
+    //   //   route: 'daftar_pesan',
+    //   //   action: 'read', 
+    //   //   resource: 'ACL',
+    //   // },
+    //   {
+    //     title: 'F.A.Q',
+    //     route: 'faq',
+    //     action: 'read', 
+    //     resource: 'ACL',
+    //   },
+    // ]
+  },
+  {
+    title: 'F.A.Q',
+    route: 'faq',
+    action: 'read', 
+    icon: 'MessageCircleIcon',
+    resource: 'ACL'
   }
 ]
